e5746dd0aeef3c5c9e40294477009a6211277a34
Ronny Roeller <ronny@collaborne.com>
2017-01-02 18:49:12 +0100

    Bugfix: sizes were not passed to PNG generation

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const transform = (sizes, imageTransform) => function(file, encoding, callback) 
 	}
 
 	const promises = Object.keys(sizes).map(name => {
-		const options = Object.assign({}, { backgroundColor: 'white' });
+		const options = Object.assign({}, SIZES[name], { backgroundColor: 'white' });
 		return imageTransform(new Buffer(file.contents), options)
 			.then(png => {
 				const result = new gutil.File({
